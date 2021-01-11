97bf7eaa6f2fbfa5c77b08c63e214a7a1ebea3a2
erwan.vivien <erwan.vivien@epita.fr>
2021-01-11 23:25:42 +0100

    added comments and good align to the printing

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ def create_access_token(client_id, client_secret, region='eu'):
     return response
 
 
+# reads the 'settings' file
 settings = json.loads(read_file("settings"))
 # print(settings)
 
@@ -54,6 +55,7 @@ realm_name = settings["realm"]
 client_id = settings["client_id"]
 client_secret = settings["client_secret"]
 
+# try creating a token
 try:
     token = create_access_token(client_id, client_secret).json()[
         "access_token"]
@@ -61,21 +63,23 @@ except:
     print("Token was not given by Blizz API")
     exit()
 
-
+# Gets the whole auction house for a realm
 r = requests.get(
     f"https://{region}.api.blizzard.com/data/wow/realm/{realm_name}?namespace=dynamic-{region}&locale=en_GB&access_token={token}")
 
+# Get the base request url
 try:
     base_url = r.json()["connected_realm"]["href"].split("?")[0]
 except:
-    print("json was bad:")
+    print("json was bad")
     print(r.text)
     exit()
-
 # print(base_url)
+
 r = requests.get(
     f"{base_url}/auctions?namespace=dynamic-{region}&locale=en_US&access_token={token}")
 
+# saving the request to region-server and region-server-date
 auctions = write_file(f"{region}-{realm_name}",
                       r.text, True)
 
@@ -83,21 +87,15 @@ auctions = write_file(f"{region}-{realm_name}",
 auctions_json = json.loads(auctions)
 ids = [int(x) for x in sys.argv[1:]]
 act_list = []
+# Parse all the AH for the items we have
 for auction in auctions_json["auctions"]:
-    # print(auction)
     if auction["item"]["id"] in ids:
-        # print(str(auction["item"]["id"]) + "\t\t" +
-        #       str(auction["quantity"]) + "\t", end='')
-
         try:
             price = auction["buyout"] / 10000
-            # print(price)
         except:
             price = auction["unit_price"] / 10000
-            # print(price)
 
         act_list += [(auction["item"]["id"], price, auction["quantity"])]
-        # print(auction)
 
 act_list.sort()
 # print(act_list)
@@ -109,7 +107,18 @@ for auction in act_list:
         r = requests.get(
             f"https://{region}.api.blizzard.com/data/wow/item/{prev}?namespace=static-{region}&locale=en_US&access_token={token}")
         print(r.json()["name"])
-    print(auction[0], "\t\t", auction[1], "\t", auction[2])
+
+    print(auction[0], end='')
+
+    mid_space = 20 - len(str(auction[0])) - len(str(auction[1]))
+    print(' ' * mid_space, end='')
+
+    print(auction[1], end='')
+
+    mid_space = 10 - len(str(auction[2]))
+    print(' ' * mid_space, end='')
+
+    print(auction[2], end='\n')
 
 
 # r = requests.get(f"https://{region}.battle.net/oauth/token")
